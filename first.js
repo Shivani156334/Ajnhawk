1230e5c5078fe2bbbabb5a2128ff434400b35662
Shivani <shivanivyavahare2002@gmail.com>
2023-06-26 21:53:48 +0530

    Initial Setup

---
+++ first.js
@@ -0,0 +1,18 @@
+const http=require('http')
+const port=8080
+
+const server=http.createServer(function(req, res){
+     res.write('Hello node')
+     res.end()
+
+})
+
+server.listen(port,function(error){
+    if (error){
+
+        console.log('something went wrong',error)
+    }else{
+        console.log('server is listening on port ' + port)
+    }
+}
+)
